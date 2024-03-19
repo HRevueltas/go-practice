4b94596b616bc598493c02cdd0c0dd74d53dd430
Harold Revueltas Gómez <105690550+HRevueltas@users.noreply.github.com>
2024-03-19 14:49:31 -0500

    Go practice

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# go-practice
+# Go Documentation
+
+Here are some useful resources for Go documentation:
+
+- [Go by Example](https://gobyexample.com/variables)
+- [Go Tutorial: Creating a Module](https://go.dev/doc/tutorial/create-module)
---
+++ quotes/quotes.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
+
+func main() {
+	fmt.Println(quote.Go())
+	fmt.Println("-----------------")
+	fmt.Println(quote.Hello())
+	fmt.Println("-----------------")
+	fmt.Println(quote.Glass())
+	fmt.Println("-----------------")
+	fmt.Println(quote.Opt())
+	fmt.Println("-----------------")
+
+}
